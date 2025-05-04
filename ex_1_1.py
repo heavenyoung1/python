4487d0f86874845424976d46271478de88196308
Eugene <jacksonou3@gmail.com>
2025-05-04 22:28:14 +0300

    реализован магический метод __init__

--- ex_1_1.py
+++ ex_1_1.py
@@ -8,3 +8,13 @@ class FrenchDeck():
         ranks.append(str(i))
     ranks += (list("JQKA"))
     print(ranks)
+
+    suits = "spades diamonds clubs hearts".split()
+    print(suits)
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+deck = FrenchDeck()  # Создаём колоду
+print(deck._cards)   # Выводим все карты
