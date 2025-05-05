dbee9ceb8aaa7b6aad0c0d45f6f52ca140f15d23
Eugene <jacksonou3@gmail.com>
2025-05-05 21:33:54 +0300

    Написание магиеских методов для класса Card

--- ex_1_1.py
+++ ex_1_1.py
@@ -3,6 +3,7 @@ import collections
 Card = collections.namedtuple("Card", ["rank", "suit"])
 ranks = []
 
+
 class FrenchDeck():
     for i in range(2, 11):
         ranks.append(str(i))
@@ -16,5 +17,17 @@ class FrenchDeck():
         self._cards = [Card(rank, suit) for suit in self.suits
                        for rank in self.ranks]
 
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
 deck = FrenchDeck()  # Создаём колоду
-print(deck._cards)   # Выводим все карты
+print(deck._cards)  # Выводим все карты
+
+for card in deck:
+    print(card)
+
+print(deck[1:10])
