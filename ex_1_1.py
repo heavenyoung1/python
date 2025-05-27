ab11c04ebce8a145dda9ff8eab96ff614f9f7281
Evgeny <jacksonou3@gmail.com>
2025-05-27 14:30:13 +0300

    изучение кортежей

--- ex_1_1.py
+++ ex_1_1.py
@@ -99,3 +99,51 @@ for color in colors:
     for size in sizes:
         print(color, size)
 
+aaa = tuple(ord(s) for s in symbols if ord(s) > 100)
+print(aaa)
+
+import array
+
+array1 = array.array("I", (ord(symbol) for symbol in symbols))
+print(array1)
+
+# Порождение декартова произведения генераторным выражением
+for tshirt in (f'{c} {s}' for c in colors for s in sizes):
+    print(tshirt)
+
+# Кортежи как записи
+
+lax_coordinates = (33.945, -54.33)
+city, year, pop, chg, area = ('Tokyo', 2003, 32_450, 0.66, 8014)
+traveler_ids = [
+    ('USA', '31195855'),
+    ('BRA', 'CE342567'),
+    ('ESP', 'XDA205856')
+]
+
+for passport in (traveler_ids):
+    print('%s/%s' % passport)
+
+for country, _ in traveler_ids:
+    print(country)
+
+# Кортежи как неизменяемые списки
+a = (10, 'alpha', [1, 2])
+b = (10, 'alpha', [1, 2])
+print(a != b)
+b[-1].append(99)
+print(a != b)
+print(b)
+
+def fixed(o):
+    try:
+        hash(o)
+    except TypeError:
+        return False
+    return True
+
+tf = (10, 'alpha', (1, 2)) # Здесь кортеж последний
+tm = (10, 'alpha', [1, 2]) # Здесь список последний
+
+print(fixed(tf))
+print(fixed(tm))
