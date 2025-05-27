1dfee5a98ef194b37b6d96befe1f38010b38645c
Evgeny <jacksonou3@gmail.com>
2025-05-27 10:07:44 +0300

    Построение декартова произведения с помощью спискового включения

--- ex_1_1.py
+++ ex_1_1.py
@@ -87,4 +87,15 @@ print(list(res))
 
 # Декартовы произведения
 
+# Построение декартова произведения с помощью спискового включения
+colors = ['black', 'white']
+
+sizes = ['S', 'M', 'L']
+
+t_shirts = [(color, size) for color in colors for size in sizes]
+print(t_shirts)
+
+for color in colors:
+    for size in sizes:
+        print(color, size)
 
