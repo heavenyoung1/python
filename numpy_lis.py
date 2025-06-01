27af00b1b424259da7c21193660742722d35467e
Evgeny <jacksonou3@gmail.com>
2025-06-01 14:17:44 +0300

    Объединение отображений оператором |

--- numpy_lis.py
+++ numpy_lis.py
@@ -17,3 +17,14 @@ print((country_dial))
 lol = {code: country.upper() for country, code in sorted((country_dial).items()) if code < 70}
 
 print(lol)
+
+def dump(**kwargs):
+    return kwargs
+
+lol2 = dump(**{'x': 1}, y=2, **{'z': 3})
+print(lol2)
+
+# Объединение отображений оператором |
+d1 = {'a': 1, 'b': 2}
+d2 = {'a': 2, 'b': 6, 'c': 4}
+print(d1 | d2)
