f1c893e2f3a3795a65b687ddf3677735df2fac8f
Evgeny <jacksonou3@gmail.com>
2025-06-01 22:59:38 +0300

    Сопоставление с объектом - образцом

--- numpy_lis.py
+++ numpy_lis.py
@@ -1,3 +1,4 @@
+
 dial_codes = [
     (880, 'Bangladesh'),
     (55, 'Brazil'),
@@ -28,3 +29,36 @@ print(lol2)
 d1 = {'a': 1, 'b': 2}
 d2 = {'a': 2, 'b': 6, 'c': 4}
 print(d1 | d2)
+#print(d1 |= d2) Вот эта хрень не работает
+
+# Сопоставление с объектом - образцом
+
+from collections import OrderedDict
+
+def get_creators(record: dict) -> list:
+    match record:
+        case {'type': 'book', 'api': 2, 'authors': [*names]}:
+            return names
+        case {'type': 'book', 'api': 1, 'author': name}:
+            return [name]
+        case {'type': 'book'}:
+            raise ValueError(f'Invalid "book" record: {record!r}')
+        case {'type': 'movie', 'director': name}:
+            return [name]
+        case _:
+            raise ValueError(f'Invalid record: {record!r}')
+
+
+b1 = dict(api=1, author='Douglas Hofstadter', type='book', title='Gödel, Escher, Bach')
+b2 = OrderedDict(api=2, type='book', title='Python in a Nutshell', authors='Martelli Ravenscroft Holden'.split())
+
+print(get_creators(b1))
+print(get_creators(b2))
+
+# Проверка шэширумости
+
+tt = (1, 2, (3, 4))
+print(hash(tt))
+
+tl = (1, 2, [3, 4])
+#hash(tl) 
